f3383c33afb660e5d5a389aba5fdad82e7f36319
Tyler Garland <tylerjgarland@gmail.com>
2022-08-04 08:43:30 -0700

    Refactor arg parsing

--- main.go
+++ main.go
@@ -3,30 +3,45 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
-	gitTokens := parseTokens(os.Args[1:])
+	gitTokens := parseTokens(os.Args)
+
+	if gitTokens.GithubToken == "" || gitTokens.GitlabToken == "" {
+		fmt.Println("No git tokens provided.")
+		gitTokens.Help = true
+	}
+
+	if gitTokens.Help {
+		fmt.Println("This tool only copies from GitLab to GitHub. It does not delete anything.")
+		fmt.Println("Create access tokens that have read access to repositories/projects and read access to the user.")
+		fmt.Println("Usage:")
+		fmt.Println("--gitlab-token <token>")
+		fmt.Println("--github-token <token>")
+		os.Exit(0)
+	}
 
 	//POC: Only Gitlab to Github export available.
 	Gitlab2Github(gitTokens.GitlabToken, gitTokens.GithubToken)
-
-	fmt.Println(gitTokens)
 }
 
 func parseTokens(arguments []string) (parsedTokens Arguments) {
 	parsedTokens = Arguments{}
-	for _, arg := range arguments {
-		splitArg := strings.Split(arg, " ")
-		switch splitArg[0] {
+	for index, arg := range arguments {
+		switch arg {
 		case "--gitlab-token":
-			parsedTokens.GitlabToken = splitArg[1]
+			parsedTokens.GitlabToken = arguments[index+1]
 			break
 		case "--github-token":
-			parsedTokens.GithubToken = splitArg[1]
+			parsedTokens.GithubToken = arguments[index+1]
+			break
+		case "-h":
+		case "--help":
+			parsedTokens.Help = true
 			break
 		}
+
 	}
 	return parsedTokens
 }
@@ -34,4 +49,5 @@ func parseTokens(arguments []string) (parsedTokens Arguments) {
 type Arguments struct {
 	GitlabToken string
 	GithubToken string
+	Help        bool
 }
