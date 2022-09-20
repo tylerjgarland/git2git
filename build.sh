0be1e889c3f1f2e093dd66e34a814d3fb4930382
Tyler Garland <tylerjgarland@gmail.com>
2022-09-20 11:02:31 -0700

    Fix typeo in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-env GOOS=linux GOARCH=amd64 go build -o builds/linux/git2git-linux-amd6464
+env GOOS=linux GOARCH=amd64 go build -o builds/linux/git2git-linux-amd64
 env GOOS=windows GOARCH=amd64 go build -o builds/windows/git2git.exe
 env GOOS=darwin GOARCH=amd64 go build -o builds/mac/git2git-mac-amd64
